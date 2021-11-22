3da57891b771bcd6d4a336f9ab99b02550f7a063
michalMilewski-8 <miles98@interia.pl>
2021-11-22 20:35:45 +0100

    Add project files.

---
+++ Block.cpp
@@ -0,0 +1,551 @@
+#include "Block.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+glm::vec3 Block::TransformToDivisions(glm::vec3 pos) const
+{
+	return {  std::round((pos.x / (double)x_size + 0.5f) * (x_divisions - 1)),std::round((pos.y / (double)y_size + 0.5f) * (y_divisions - 1)),pos.z };
+}
+
+void Block::SetViewPos(glm::vec3 view_pos)
+{
+	this->view_pos = view_pos;
+}
+
+void Block::Update()
+{
+	need_update = true;
+}
+
+float Block::GetHeight(int x, int y)
+{
+	
+	if (x >= 0 && x < x_divisions && y >= 0 && y < y_divisions)
+		return data[y * x_divisions + x];
+	else
+		return -100.0f;
+}
+
+void Block::SetHeight(int x, int y, float val)
+{
+	if (x >= 0 && x < x_divisions && y >= 0 && y < y_divisions)
+		data[y * x_divisions + x] = val;
+}
+
+void Block::DrawFrame(float T, glm::vec3 start_pos, glm::vec3 end_pos, glm::vec3 rotation_start, glm::vec3 rotation_end, bool aproximation_is_line)
+{
+	rotate = glm::mat4(0.0f);
+
+	if (aproximation_is_line) {
+		this->MoveObjectTo(start_pos + (end_pos - start_pos) * T);
+		this->RotateObject(EulerToQuaternion(rotation_start + (rotation_end - rotation_start) * T));
+	}
+}
+
+void Block::DrawFrame(float T, glm::vec3 start_pos, glm::vec3 end_pos, glm::quat rotation_start, glm::quat rotation_end, bool aproximation_is_line)
+{
+	rotate = glm::mat4(0.0f);
+
+	if (aproximation_is_line) {
+		this->MoveObjectTo(start_pos + (end_pos - start_pos) * T);
+		this->RotateObject(rotation_start + (rotation_end - rotation_start) * T);
+	}
+}
+
+void Block::create_block_points()
+{
+
+		// Point xyz // 0
+		points.push_back(0);
+		points.push_back(-y_size);
+		points.push_back(z_size);
+		// Normal xyz
+		points.push_back(0.0f);
+		points.push_back(0.0f);
+		points.push_back(1.0f);
+
+		// Point xyz // 4
+		points.push_back(0);
+		points.push_back(0);
+		points.push_back(z_size);
+		// Normal xyz
+		points.push_back(0.0f);
+		points.push_back(0.0f);
+		points.push_back(1.0f);
+
+		// Point xyz // 1
+		points.push_back(x_size);
+		points.push_back(-y_size);
+		points.push_back(z_size);
+		// Normal xyz
+		points.push_back(0.0f);
+		points.push_back(0.0f);
+		points.push_back(1.0f);
+
+		// Point xyz // 1
+		points.push_back(x_size);
+		points.push_back(-y_size);
+		points.push_back(z_size);
+		// Normal xyz
+		points.push_back(0.0f);
+		points.push_back(0.0f);
+		points.push_back(1.0f);
+
+		// Point xyz // 4
+		points.push_back(0);
+		points.push_back(0);
+		points.push_back(z_size);
+		// Normal xyz
+		points.push_back(0.0f);
+		points.push_back(0.0f);
+		points.push_back(1.0f);
+
+		// Point xyz // 5
+		points.push_back(x_size);
+		points.push_back(0);
+		points.push_back(z_size);
+		// Normal xyz
+		points.push_back(0.0f);
+		points.push_back(0.0f);
+		points.push_back(1.0f);
+
+		// Point xyz // 1
+		points.push_back(x_size);
+		points.push_back(-y_size);
+		points.push_back(z_size);
+		// Normal xyz
+		points.push_back(1.0f);
+		points.push_back(0.0f);
+		points.push_back(0.0f);
+
+		// Point xyz // 5
+		points.push_back(x_size);
+		points.push_back(0);
+		points.push_back(z_size);
+		// Normal xyz
+		points.push_back(1.0f);
+		points.push_back(0.0f);
+		points.push_back(0.0f);
+
+		// Point xyz // 6
+		points.push_back(x_size);
+		points.push_back(0);
+		points.push_back(0);
+		// Normal xyz
+		points.push_back(1.0f);
+		points.push_back(0.0f);
+		points.push_back(0.0f);
+
+		// Point xyz // 1
+		points.push_back(x_size);
+		points.push_back(-y_size);
+		points.push_back(z_size);
+		// Normal xyz
+		points.push_back(1.0f);
+		points.push_back(0.0f);
+		points.push_back(0.0f);
+
+		// Point xyz // 6
+		points.push_back(x_size);
+		points.push_back(0);
+		points.push_back(0);
+		// Normal xyz
+		points.push_back(1.0f);
+		points.push_back(0.0f);
+		points.push_back(0.0f);
+
+		// Point xyz // 2
+		points.push_back(x_size);
+		points.push_back(-y_size);
+		points.push_back(0);
+		// Normal xyz
+		points.push_back(1.0f);
+		points.push_back(0.0f);
+		points.push_back(0.0f);
+
+		// Point xyz // 0
+		points.push_back(0);
+		points.push_back(-y_size);
+		points.push_back(z_size);
+		// Normal xyz
+		points.push_back(-1.0f);
+		points.push_back(0.0f);
+		points.push_back(0.0f);
+
+		// Point xyz // 3
+		points.push_back(0);
+		points.push_back(-y_size);
+		points.push_back(0);
+		// Normal xyz
+		points.push_back(-1.0f);
+		points.push_back(0.0f);
+		points.push_back(0.0f);
+
+		// Point xyz // 4
+		points.push_back(0);
+		points.push_back(0);
+		points.push_back(z_size);
+		// Normal xyz
+		points.push_back(-1.0f);
+		points.push_back(0.0f);
+		points.push_back(0.0f);
+
+
+		// Point xyz // 3
+		points.push_back(0);
+		points.push_back(-y_size);
+		points.push_back(0);
+		// Normal xyz
+		points.push_back(-1.0f);
+		points.push_back(0.0f);
+		points.push_back(0.0f);
+
+		// Point xyz // 7
+		points.push_back(0);
+		points.push_back(0);
+		points.push_back(0);
+		// Normal xyz
+		points.push_back(-1.0f);
+		points.push_back(0.0f);
+		points.push_back(0.0f);
+
+		// Point xyz // 4
+		points.push_back(0);
+		points.push_back(0);
+		points.push_back(z_size);
+		// Normal xyz
+		points.push_back(-1.0f);
+		points.push_back(0.0f);
+		points.push_back(0.0f);
+
+
+		// Point xyz // 7
+		points.push_back(0);
+		points.push_back(0);
+		points.push_back(0);
+		// Normal xyz
+		points.push_back(0.0f);
+		points.push_back(0.0f);
+		points.push_back(-1.0f);
+
+		// Point xyz // 3
+		points.push_back(0);
+		points.push_back(-y_size);
+		points.push_back(0);
+		// Normal xyz
+		points.push_back(0.0f);
+		points.push_back(0.0f);
+		points.push_back(-1.0f);
+
+		// Point xyz // 6
+		points.push_back(x_size);
+		points.push_back(0);
+		points.push_back(0);
+		// Normal xyz
+		points.push_back(0.0f);
+		points.push_back(0.0f);
+		points.push_back(-1.0f);
+
+		// Point xyz // 6
+		points.push_back(x_size);
+		points.push_back(0);
+		points.push_back(0);
+		// Normal xyz
+		points.push_back(0.0f);
+		points.push_back(0.0f);
+		points.push_back(-1.0f);
+
+		// Point xyz // 3
+		points.push_back(0);
+		points.push_back(-y_size);
+		points.push_back(0);
+		// Normal xyz
+		points.push_back(0.0f);
+		points.push_back(0.0f);
+		points.push_back(-1.0f);
+
+		// Point xyz // 2
+		points.push_back(x_size);
+		points.push_back(-y_size);
+		points.push_back(0);
+		// Normal xyz
+		points.push_back(0.0f);
+		points.push_back(0.0f);
+		points.push_back(-1.0f);
+
+
+		// Point xyz // 4
+		points.push_back(0);
+		points.push_back(0);
+		points.push_back(z_size);
+		// Normal xyz
+		points.push_back(0.0f);
+		points.push_back(1.0f);
+		points.push_back(0.0f);
+
+		// Point xyz // 7
+		points.push_back(0);
+		points.push_back(0);
+		points.push_back(0);
+		// Normal xyz
+		points.push_back(0.0f);
+		points.push_back(1.0f);
+		points.push_back(0.0f);
+
+		// Point xyz // 6
+		points.push_back(x_size);
+		points.push_back(0);
+		points.push_back(0);
+		// Normal xyz
+		points.push_back(0.0f);
+		points.push_back(1.0f);
+		points.push_back(0.0f);
+
+
+		// Point xyz // 5
+		points.push_back(x_size);
+		points.push_back(0);
+		points.push_back(z_size);
+		// Normal xyz
+		points.push_back(1.0f);
+		points.push_back(0.0f);
+		points.push_back(0.0f);
+
+		// Point xyz // 4
+		points.push_back(0);
+		points.push_back(0);
+		points.push_back(z_size);
+		// Normal xyz
+		points.push_back(0.0f);
+		points.push_back(1.0f);
+		points.push_back(0.0f);
+
+		// Point xyz // 6
+		points.push_back(x_size);
+		points.push_back(0);
+		points.push_back(0);
+		// Normal xyz
+		points.push_back(0.0f);
+		points.push_back(1.0f);
+		points.push_back(0.0f);
+
+
+		// Point xyz // 0
+		points.push_back(0);
+		points.push_back(-y_size);
+		points.push_back(z_size);
+		// Normal xyz
+		points.push_back(0.0f);
+		points.push_back(-1.0f);
+		points.push_back(0.0f);
+
+		// Point xyz // 1
+		points.push_back(x_size);
+		points.push_back(-y_size);
+		points.push_back(z_size);
+		// Normal xyz
+		points.push_back(0.0f);
+		points.push_back(-1.0f);
+		points.push_back(0.0f);
+
+		// Point xyz // 3
+		points.push_back(0);
+		points.push_back(-y_size);
+		points.push_back(0);
+		// Normal xyz
+		points.push_back(0.0f);
+		points.push_back(-1.0f);
+		points.push_back(0.0f);
+
+
+		// Point xyz // 1
+		points.push_back(x_size);
+		points.push_back(-y_size);
+		points.push_back(z_size);
+		// Normal xyz
+		points.push_back(0.0f);
+		points.push_back(-1.0f);
+		points.push_back(0.0f);
+
+		// Point xyz // 2
+		points.push_back(x_size);
+		points.push_back(-y_size);
+		points.push_back(0);
+		// Normal xyz
+		points.push_back(0.0f);
+		points.push_back(-1.0f);
+		points.push_back(0.0f);
+
+		// Point xyz // 3
+		points.push_back(0);
+		points.push_back(-y_size);
+		points.push_back(0);
+		// Normal xyz
+		points.push_back(0.0f);
+		points.push_back(-1.0f);
+		points.push_back(0.0f);
+
+
+
+	// Indexing triangles
+	for (int i = 0; i < points.size(); i++) {
+		quads.push_back(i);
+	}
+
+	blocks_need_creation = false;
+}
+
+void Block::update_object()
+{
+	if (blocks_need_creation) {
+		quads.clear();
+		points.clear();
+
+		create_block_points();
+
+		shader.use();
+		// 1. bind Vertex Array Object
+		glBindVertexArray(VAO);
+		// 2. copy our vertices array in a vertex buffer for OpenGL to use
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(float) * points.size(), points.data(), GL_DYNAMIC_DRAW);
+		// 3. copy our index array in a element buffer for OpenGL to use
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * quads.size(), quads.data(), GL_DYNAMIC_DRAW);
+		// 4. then set the vertex attributes pointers
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, description_number * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, description_number * sizeof(float), (void*)(3 * sizeof(float)));
+		glEnableVertexAttribArray(1);
+
+		int x_deltaPos = glGetUniformLocation(shader.ID, "x_delta");
+		glUniform1f(x_deltaPos, x_delta);
+
+		int y_deltaPos = glGetUniformLocation(shader.ID, "y_delta");
+		glUniform1f(y_deltaPos, y_delta);
+
+		int x_sizePos = glGetUniformLocation(shader.ID, "x_size");
+		glUniform1f(x_sizePos, x_size);
+
+		int y_sizePos = glGetUniformLocation(shader.ID, "y_size");
+		glUniform1f(y_sizePos, y_size);
+
+		int z_sizePos = glGetUniformLocation(shader.ID, "z_size");
+		glUniform1f(z_sizePos, z_size);
+
+		blocks_need_creation = false;
+	}
+
+}
+
+Block::Block(float x_size_, float y_size_, float z_size_, int x_divisions_, int y_divisions_, Shader sh) :
+	Object(sh, 6)
+{
+	x_size = x_size_;
+	y_size = y_size_;
+	z_size = z_size_;
+	x_divisions = x_divisions_;
+	y_divisions = y_divisions_;
+
+	shader = Shader("shader_tex.vs", "shader_tex.fs");
+	update_object();
+	shader.use();
+}
+
+void Block::DrawObject(glm::mat4 mvp)
+{
+	if (need_update) {
+		update_object();
+		need_update = false;
+	}
+
+	Object::DrawObject(mvp);
+
+	glm::mat4 model = translate * rotate * resize;
+	glm::mat4 vp = mvp;
+	shader.use();
+	glm::mat4 trmodel = glm::transpose(glm::inverse(model));
+	int projectionLoc = glGetUniformLocation(shader.ID, "model");
+	glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+	int trmodelLoc = glGetUniformLocation(shader.ID, "trmodel");
+	glUniformMatrix4fv(trmodelLoc, 1, GL_FALSE, glm::value_ptr(trmodel));
+
+	int mvLoc = glGetUniformLocation(shader.ID, "vp");
+	glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(vp));
+
+	int viewPos = glGetUniformLocation(shader.ID, "viewPos");
+	glUniform3fv(viewPos, 1, &view_pos[0]);
+	
+	int colorPos = glGetUniformLocation(shader.ID, "color");
+	
+
+	color = { 0.7,0.7,0 };
+	glUniform3fv(colorPos, 1, &color[0]);
+
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	glDrawElements(GL_TRIANGLES, quads.size(), GL_UNSIGNED_INT, 0);
+
+	color = { 1,1,1 };
+	glUniform3fv(colorPos, 1, &color[0]);
+	glLineWidth(5.0f);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	glDrawElements(GL_TRIANGLES, quads.size(), GL_UNSIGNED_INT, 0);
+	glBindVertexArray(0);
+	
+}
+
+glm::quat Block::EulerToQuaternion(glm::vec3 rot)
+{
+	//glm::vec3 Ox = { 1,0,0 };
+	//glm::vec3 Oy = { 0,1,0 };
+	//glm::vec3 Oz = { 0,0,1 };
+
+	//rot = rot / (float)(180.0f * M_PI);
+
+	//glm::quat quat = {0,0,0,1};
+	//auto q = glm::angleAxis(rot.x, Oz);
+	//Ox = q * Ox;
+	//Oy = q * Oy;
+	//Oz = q * Oz;
+	//quat = q * quat;
+
+	//q = glm::angleAxis(rot.y, Ox);
+	//Ox = q * Ox;
+	//Oy = q * Oy;
+	//Oz = q * Oz;
+	//quat = q * quat;
+
+	//q = glm::angleAxis(rot.z, Oz);
+	//Ox = q * Ox;
+	//Oy = q * Oy;
+	//Oz = q * Oz;
+	//quat = q * quat;
+
+	//return quat;
+
+	rot = rot / (float)(180.0f * M_PI);
+	// Abbreviations for the various angular functions
+	double yaw = rot.x;
+	double pitch = rot.y;
+	double roll = rot.z;
+	double cy = cos(yaw * 0.5);
+	double sy = sin(yaw * 0.5);
+	double cp = cos(pitch * 0.5);
+	double sp = sin(pitch * 0.5);
+	double cr = cos(roll * 0.5);
+	double sr = sin(roll * 0.5);
+
+	glm::quat q;
+	q.w = cr * cp * cy + sr * sp * sy;
+	q.x = sr * cp * cy - cr * sp * sy;
+	q.y = cr * sp * cy + sr * cp * sy;
+	q.z = cr * cp * sy - sr * sp * cy;
+
+	return q;
+}
+
+glm::vec3 Block::QuaternionToEuler(glm::quat quat)
+{
+	return (glm::eulerAngles(quat) / (float)M_PI) * 180.0f;
+}
---
+++ main.cpp
@@ -0,0 +1,319 @@
+#include <stdio.h>
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtc/quaternion.hpp>
+#include <glm/gtx/quaternion.hpp>
+#include <math.h>
+#include <vector>
+
+#include "Block.h"
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+#include "Dependencies/include/rapidxml-1.13/rapidxml.hpp"
+#include "Dependencies/include/rapidxml-1.13/rapidxml_print.hpp"
+#include "Dependencies/include/rapidxml-1.13/rapidxml_utils.hpp"
+#include "./Dependencies/include/ImGuiFileDialog-Lib_Only/ImGuiFileDialog.h"
+
+#define DEFAULT_WIDTH 1280
+#define DEFAULT_HEIGHT 720
+#define EPS 0.1
+#define PRECISION 1.0f
+using namespace rapidxml;
+using namespace std;
+
+glm::vec3 cameraPos, cameraFront, cameraUp, lookAt, moving_up;
+unsigned int width_, height_;
+
+int e = 0;
+glm::mat4 projection, view, model, mvp;
+glm::mat4 projection_i, view_i, model_i, mvp_i;
+glm::vec2 mousePosOld, angle;
+float deltaTime = 0.0f;	// Time between current frame and last frame
+float lastFrame = 0.0f; // Time of last frame
+float ipd = 0.01f;
+float d = 0.1f;
+float near = 0.01f;
+float far = 20.0f;
+bool animate = false;
+float T = 0.0f;
+float animation_time = 1.0f;
+
+float distance_d = 1.0f;
+float speed = 0.2f; // speed of milling tool in milimeters per second;
+float size_x = 1;
+float size_y = 1;
+float size_z = 1;
+int divisions_x = 400;
+int divisions_y = 400;
+glm::vec3 translation_s;
+glm::vec3 translation_e;
+glm::vec3 rot_euler_s;
+glm::vec3 rot_euler_e;
+glm::quat quaternion_s = {0,0,0,1};
+glm::quat quaternion_e = {0,0,0,1};
+bool is_linear_aprox = true;
+
+Camera cam;
+Shader ourShader;
+Shader ourShader2;
+GLFWwindow* window;
+GLFWwindow* window2;
+//std::unique_ptr<Cursor> cursor, center;
+
+std::vector<std::shared_ptr<Object>> objects_list = {};
+std::unique_ptr<Block> block;
+std::unique_ptr<Block> block2;
+
+void draw_scene();
+void draw_scene2();
+void framebuffer_size_callback(GLFWwindow* window_1, int width, int height);
+void framebuffer_size_callback2(GLFWwindow* window_1, int width, int height);
+void processInput(GLFWwindow* window);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void create_gui();
+
+int main() {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	window = glfwCreateWindow(DEFAULT_WIDTH, DEFAULT_HEIGHT, "baczek 1", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	model = glm::mat4(1.0f);
+	view = glm::mat4(1.0f);
+
+	cameraPos = { 0,0,5 };
+	cameraFront = { 0,0,-1 };
+	lookAt = { 0,0,0 };
+	cameraUp = { 0,1,0 };
+
+	angle = { -90.0f, 0.0f };
+	width_ = DEFAULT_WIDTH;
+	height_ = DEFAULT_HEIGHT;
+
+	cam = Camera(cameraPos, cameraFront, cameraUp);
+	cam.SetPerspective(glm::radians(45.0f), DEFAULT_WIDTH / (float)DEFAULT_HEIGHT, near, far);
+
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	glViewport(0, 0, DEFAULT_WIDTH, DEFAULT_HEIGHT);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
+
+	// Setup Dear ImGui style
+	ImGui::StyleColorsDark();
+	//ImGui::StyleColorsClassic();
+	const char* glsl_version = "#version 330";
+	// Setup Platform/Renderer bindings
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init(glsl_version);
+
+	// build and compile our shader program
+	// ------------------------------------
+	ourShader = Shader("shader.vs", "shader.fs"); // you can name your shader files however you like
+
+	glEnable(GL_DEPTH_TEST);
+
+	block = std::make_unique<Block>(size_x, size_y, size_z, divisions_x, divisions_y, ourShader);
+	
+	// render loop
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwMakeContextCurrent(window);
+		// cleaning frame
+		glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		projection = cam.GetProjectionMatrix();
+		projection_i = glm::inverse(projection);
+		view = cam.GetViewMatrix();
+		view_i = glm::inverse(view);
+
+		mvp = projection * view;
+
+		float currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
+		processInput(window);
+		create_gui();
+
+		draw_scene();
+
+		// Render dear imgui into screen
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		// check and call events and swap the buffers
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+
+		if (animate) {
+			T += deltaTime / animation_time;
+		}
+	}
+
+	// cleanup stuff
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+
+	glfwTerminate();
+	objects_list.clear();
+	ourShader.deleteShader();
+	return 0;
+}
+
+void draw_scene() {
+	//block->DrawFrame(T, translation_s, translation_e, quaternion_s, quaternion_e, is_linear_aprox);
+	block->DrawObject(mvp);
+}
+
+#pragma region  boilerCodeOpenGL
+
+void framebuffer_size_callback(GLFWwindow* window_1, int width, int height)
+{
+	glfwMakeContextCurrent(window_1);
+	glViewport(0, 0, width, height);
+
+	cam.SetPerspective(glm::radians(45.0f), width / (float)height, near, far);
+	width_ = width;
+	height_ = height;
+}
+
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (ImGui::GetIO().WantCaptureMouse)
+		return;
+	glm::vec2 mousePos = { xpos,ypos };
+	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_MIDDLE) == GLFW_PRESS)
+	{
+		glm::vec2 diff = (mousePosOld - mousePos) * PRECISION;
+		float cameraSpeed = 5 * deltaTime;
+		float radius;
+		diff *= cameraSpeed;
+
+		glm::vec3 right_movement = cam.GetRightVector() * -diff.x;
+		glm::vec3 up_movement = cam.GetUpVector() * diff.y;
+		glm::vec3 angle2 = lookAt - (cameraPos + right_movement + up_movement);
+
+		auto rotation = Object::RotationBetweenVectors(lookAt - cameraPos, angle2);
+		auto roation = glm::toMat4(rotation);
+		angle += diff;
+		if (angle.y > 90.0f) angle.y = 90.0f - EPS;
+		if (angle.y < -90.0f) angle.y = -90.0f + EPS;
+		if (angle.x > 180.0f) angle.x = -180.0f + EPS;
+		if (angle.x < -180.0f) angle.x = 180.0f - EPS;
+		radius = glm::length(cameraPos - lookAt);
+
+		cameraPos.x = lookAt.x + radius * glm::cos(glm::radians(angle.y)) * glm::cos(glm::radians(angle.x));
+		cameraPos.z = lookAt.z + radius * -glm::cos(glm::radians(angle.y)) * glm::sin(glm::radians(angle.x));
+		cameraPos.y = lookAt.y + radius * glm::sin(glm::radians(-angle.y));
+
+		cameraFront = glm::normalize(lookAt - cameraPos);
+		cam.LookAt(cameraPos, cameraFront, cameraUp);
+	}
+	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS)
+	{
+		glm::vec2 diff = (mousePosOld - mousePos) * PRECISION;
+		float cameraSpeed = speed * deltaTime;
+
+		glm::vec2 movement = diff * cameraSpeed;
+
+		glm::vec3 right_movement = cam.GetRightVector() * movement.x;
+		glm::vec3 up_movement = cam.GetUpVector() * -movement.y;
+		cameraPos += right_movement + up_movement;
+		lookAt += right_movement + up_movement;
+
+		cam.LookAt(cameraPos, cameraFront, cameraUp);
+	}
+	mousePosOld = mousePos;
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	if (ImGui::GetIO().WantCaptureMouse)
+		return;
+
+	float precision = 0.01f;
+
+	float movement = 1.0f - yoffset * precision;
+	if (movement <= 0.0f)
+		movement = 0.1f;
+	cameraFront = glm::normalize(lookAt - cameraPos);
+	float dist = glm::length(lookAt - cameraPos);
+	cameraPos = lookAt - (cameraFront * dist * movement);
+	cam.LookAt(cameraPos, cameraFront, cameraUp);
+
+}
+
+#pragma endregion
+
+void create_gui() {
+	ImGui_ImplOpenGL3_NewFrame();
+	ImGui_ImplGlfw_NewFrame();
+	ImGui::NewFrame();
+	//ImGui::ShowDemoWindow();
+	bool open;
+	ImGuiWindowFlags flags = 0;
+	//flags |= ImGuiWindowFlags_MenuBar;
+	ImGui::Begin("Main Menu##uu", &open, flags);
+
+	ImGui::InputFloat3("Start Position", (float*)&translation_s);
+	ImGui::InputFloat3("End Position", (float*)&translation_e);
+	if (ImGui::InputFloat3("Euler rotation start", (float*)&rot_euler_s)) quaternion_s = Block::EulerToQuaternion(rot_euler_s);
+	if (ImGui::InputFloat3("Euler rotation end", (float*)&rot_euler_e)) quaternion_e = Block::EulerToQuaternion(rot_euler_e);
+	if (ImGui::InputFloat4("Quaternion start", (float*)&quaternion_s)) {
+		quaternion_s = glm::normalize(quaternion_s);
+		rot_euler_s = Block::QuaternionToEuler(quaternion_s); 
+	}
+	if (ImGui::InputFloat4("Quaternion end", (float*)&quaternion_e)) {
+		quaternion_e = glm::normalize(quaternion_e);
+		rot_euler_e = Block::QuaternionToEuler(quaternion_e);
+	};
+
+	ImGui::SliderFloat("Animation time", &animation_time, 0.1f, 100.0f, "%.3f", 1.0f);
+
+	if (ImGui::Button("Start Animation")) animate = true;
+	if (ImGui::Button("Stop Animation")) animate = false;
+	if (ImGui::Button("Restart Animation")) T = 0.0f;
+
+	ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+	ImGui::End();
+}
